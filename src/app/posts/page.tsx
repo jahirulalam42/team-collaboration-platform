f3796e8bbc70f33310f803badb20c97a1c9432dc
jahirul <jahirulalam41@gmail.com>
2026-06-09 17:09:22 +0600

    changed style

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -16,10 +16,10 @@ export default async function Posts() {
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16 text-[#333333]">
-      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl font-bold mb-8 font-(family-name:--font-geist-sans)">
         Posts
       </h1>
-      <ul className="font-[family-name:var(--font-geist-sans)] max-w-2xl space-y-4">
+      <ul className="font-(family-name:--font-geist-sans) max-w-2xl space-y-4">
         {posts.map(
           (post: {
             id: Key | null | undefined;
