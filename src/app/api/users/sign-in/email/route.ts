2334fee419f8c01119946dd1f938c541b0db9e17
jahirul <jahirulalam41@gmail.com>
2026-06-10 17:03:27 +0600

    Added Better Auth

---
+++ src/app/api/users/sign-in/email/route.ts
@@ -0,0 +1,20 @@
+import { auth } from "../../../../../../lib/auth";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const result = await auth.api.signInEmail({
+    body: {
+      email: body.email, // required
+      password: body.password, // required
+      rememberMe: true,
+      callbackURL: "/",
+    },
+    // This endpoint requires session cookies.
+    // headers: await headers(),
+  });
+
+  return Response.json(result, {
+    status: 201,
+  });
+}
